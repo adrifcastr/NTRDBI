d6bdf7bce2a59d41c56d0235e7f66566aae32e59
Jolty <juplagon@gmail.com>
2017-03-06 12:24:14 +0100

    installtitledb.c: re-draw all original UI with download method
    It have still some bugs, but because of I'm a bit bussy in this moment,
    I'll upload to github to have it as a backup. This is a WIP and I'm
    still working on it!
    
    Additions:
    -When user press Install, a dialog ask if user want to download the
    plugin. Click NO to return to title list. When pressing A, plugin will
    download. NTRDBI will notify when download finished (exceptions aren't
    handle right now because this is a WIP and not suitable for release yet.
    
    Bugs (they aren't actually)
    - Okay (Any button) -> keeps downloading.
    Actually this isn't a bug. It's because I haven't handle it either.
    
    I'm still working on this file and there are a lot of things more to
    implement here!

--- source/ui/section/action/installtitledb.c
+++ source/ui/section/action/installtitledb.c
@@ -18,17 +18,18 @@ char file[0x100];
 char pluginURL[0x100];
 
 void action_install_titledb(linked_list* items, list_item* selected) {
-    char url[0x100];
-    snprintf(url, sizeof(url), "%s", ((titledb_info*) selected->data)->downloadURL);
-    downloadPlugin("Install the selected title from NTRDB?", url, selected);
+	internal_downloadPlugin_start("Download the selected plugin from NTRDB?", selected);
 }
 
-void downloadPlugin(const char* text, const char* url, list_item* selected) {
-	/** This method will be used to download the .plg and store in the desired location. **/
+void internal_downloadPlugin_start(const char* text, list_item* selected) {
+	/** This method will prepare the download path and the download URL **/
 
 	// Read the plugin data first.
 	titledb_info* titledbInfo = (titledb_info*) selected->data;
 
+	// Store the download URL.
+	snprintf(pluginURL, sizeof(pluginURL), "%s", titledbInfo->downloadURL);
+	
 	// Create the folder for plugin.
 	u64 titleID = titledbInfo->titleId;
 	char titleID_s[] = {"0123456789ABCDEF"}; // dummy titleID. TODO check if this titleID folder is generated. if true, there is an error!
@@ -38,112 +39,28 @@ void downloadPlugin(const char* text, const char* url, list_item* selected) {
 	fsInit();
 	char dir[50];
 	snprintf(dir, sizeof(dir), "sdmc:/plugin/%s", titleID_s);
-	dir[49] = '\0';
+	dir[sizeof(dir) - 1] = '\0';
 	mkdir(dir, 0777);
 	fsExit();
 	
-	// DownloadFile wants file to be "/plugins/TID/name"
-	char tmp[0x100];
-	//snprintf(tmp, sizeof(tmp), "/plugin/%s/%s", titleID_s, titledbInfo->meta.shortDescription);
-	snprintf(tmp, sizeof(tmp), "/plugin/%s/%s.plg", titleID_s, titledbInfo->meta.shortDescription);
+	// DownloadFile wants file to be "/plugin/TID/name.plg"
+	snprintf(file, sizeof(file), "/plugin/%s/%s.plg", titleID_s, titledbInfo->meta.shortDescription);
 
-	// Store
-	///strncpy(file, tmp, sizeof(titleID));
-	///strncpy(pluginURL, url, sizeof(pluginURL));
-	///file[255] = '\0';
-
-	// Download: TODO fix the code above this to make all UI WORKS!!. This is a TEMP solution!
-	
-	char* downloadURL = strdup(url);
-	fsInit();
-	httpcInit(0x1000);
-	httpcContext context;
-	u32 statuscode=0;
-	HTTPC_RequestMethod useMethod = HTTPC_METHOD_GET;
-
-	do {
-		if (statuscode >= 301 && statuscode <= 308) {
-			char newurl[4096];
-			httpcGetResponseHeader(&context, (char*)"Location", &newurl[0], 4096);
-			downloadURL = &newurl[0];
-			httpcCloseContext(&context);
-		}
-
-		Result ret = httpcOpenContext(&context, useMethod, downloadURL, 0);
-		httpcSetSSLOpt(&context, SSLCOPT_DisableVerify);
-
-		if (ret==0) {
-			if(R_SUCCEEDED(httpcBeginRequest(&context))){
-				u32 contentsize=0;
-				if(R_FAILED(httpcGetResponseStatusCode(&context, &statuscode))){
-					httpcCloseContext(&context);
-					httpcExit();
-					fsExit();
-					return;
-				}
-				if (statuscode == 200) {
-					u32 readSize = 0;
-					long int bytesWritten = 0;
-
-					Handle fileHandle;
-					FS_Path filePath=fsMakePath(PATH_ASCII, tmp);
-					FSUSER_OpenFileDirectly(&fileHandle, ARCHIVE_SDMC, fsMakePath(PATH_EMPTY, ""), filePath, FS_OPEN_CREATE | FS_OPEN_WRITE, 0x00000000);
-
-					if(R_FAILED(httpcGetDownloadSizeState(&context, NULL, &contentsize))){
-						httpcCloseContext(&context);
-						httpcExit();
-						fsExit();
-						return;
-					}
-					u8* buf = (u8*)malloc(contentsize);
-					memset(buf, 0, contentsize);
-
-					do {
-						if(R_FAILED(ret = httpcDownloadData(&context, buf, contentsize, &readSize))){
-							free(buf);
-							httpcCloseContext(&context);
-							httpcExit();
-							fsExit();
-							return;
-						}
-						FSFILE_Write(fileHandle, NULL, bytesWritten, buf, readSize, 0x10001);
-						bytesWritten += readSize;
-					} while (ret == (s32)HTTPC_RESULTCODE_DOWNLOADPENDING);
-
-					FSFILE_Close(fileHandle);
-					svcCloseHandle(fileHandle);
-
-					free(buf);
-				}
-			}else{
-				httpcCloseContext(&context);
-				httpcExit();
-				fsExit();
-				return;
-			}
-		}else{
-			httpcCloseContext(&context);
-			httpcExit();
-			fsExit();
-			return;
-		}
-	} while ((statuscode >= 301 && statuscode <= 303) || (statuscode >= 307 && statuscode <= 308));
-	httpcCloseContext(&context);
-
-	httpcExit();
-	fsExit();
-	
-	/// Show the confirmation text
-	///prompt_display("Confirmation", text, COLOR_TEXT, true, (titledb_info*) selected->data, NULL, downloadPlugin_confirmed);
-}
-/*
-void downloadPlugin_confirmed(ui_view* view, void* data, bool response) {
-   info_display("Downloading plugin", "Press B to cancel.", false, data, startDownload, NULL);
+	// Show the confirmation text
+	prompt_display("Confirmation", text, COLOR_TEXT, true, titledbInfo, NULL, internal_downloadPlugin_confirmed);
 }
 
-void startDownload(ui_view* view, void* data, float* progress, char* text){
 
-	char* downloadURL = pluginURL;
+void internal_downloadPlugin_confirmed(ui_view* view, void* data, bool response) {
+	if(response) {
+		info_display("Downloading plugin", "Press B to cancel.", false, data, internal_downloadPlugin_download, NULL);
+	}
+}
+
+
+void internal_downloadPlugin_download(ui_view* view, void* data, float* progress, char* text){
+
+	char* downloadURL = strdup(pluginURL);
 	fsInit();
 	httpcInit(0x1000);
 	httpcContext context;
@@ -221,8 +138,7 @@ void startDownload(ui_view* view, void* data, float* progress, char* text){
 
 	httpcExit();
 	fsExit();
-	info_destroy(view);
 
 	prompt_display("Success", "Download finished.", COLOR_TEXT, false, NULL, NULL, NULL);
 	return;
-}*/
+}
--- source/ui/section/action/installtitledb.h
+++ source/ui/section/action/installtitledb.h
@@ -2,7 +2,7 @@
 
 #include "../../info.h"
 void action_install_titledb(linked_list* items, list_item* selected);
-void downloadPlugin(const char* text, const char* url, list_item* selected);
-void downloadPlugin_confirmed(ui_view* view, void* data, bool response);
-void startDownload(ui_view* view, void* data, float* progress, char* text);
+void internal_downloadPlugin_start(const char* text, list_item* selected);
+void internal_downloadPlugin_confirmed(ui_view* view, void* data, bool response);
+void internal_downloadPlugin_download(ui_view* view, void* data, float* progress, char* text);
 
