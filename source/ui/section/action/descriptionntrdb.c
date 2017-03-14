91bddddd2f2c11642ca8d856db2054fc136afe8f
Jolty <Jolty95@users.noreply.github.com>
2017-03-14 16:02:52 +0100

    descriptionntrdb.c: try to show description text in top screen
    I will need help to scroll down the text (but when all it's fnished!)

--- source/ui/section/action/descriptionntrdb.c
+++ source/ui/section/action/descriptionntrdb.c
@@ -24,8 +24,8 @@ void description_ntrdb_open(linked_list* items, list_item* selected) {
 
         return;
     }
-
-    list_display(data->meta.name, "B: Return", NULL, ntrdb_update, NULL);
+	
+    list_display(data->meta.name, "B: Return", NULL, ntrdb_update, ntrdb_draw_top);
 }
 
 static void ntrdb_update(ui_view* view, void* data, linked_list* items, list_item* selected, bool selectedTouched) {
@@ -33,10 +33,24 @@ static void ntrdb_update(ui_view* view, void* data, linked_list* items, list_ite
         ui_pop();
         return;
     }
-	
-	
 }
 
 static void ntrdb_draw_top(ui_view* view, void* data, float x1, float y1, float x2, float y2, list_item* selected) {
-    ui_draw_ntrdb_info(view, selected->data, x1, y1, x2, y2);
+	//ntrdb_info* info = (ntrdb_info*) selected->data;
+	
+	char str[] = "This cheat-menu includes:\r\n\r\n1. Text to Item\r\n2. Duplication\r\n3. Moon Jump\r\n4. Coordinates Modifier\r\n5. Teleport\r\n6. Speed Hack\r\n7. Seeder (Set, Destroy, Undo)\r\n8. Search and Replace\r\n9. Instant Tree\r\n10. Destroy All Weeds\r\n11. Water All Flowers\r\n12. Grass\r\n13. Desert\r\n14. Nookling Upgrades\r\n15. Tan Modifier\r\n16. Real Time World Edit\r\n17. Time Travel\r\n18. Time Machine ";
+
+	int i;
+	for(i = 0; str[i] != '\0'; i++) {
+		if((str[i] == '\r') && (str[i + 1] == '\n')) {
+			str[i] = '\n';
+		}
+	}
+	for(i = 0; str[i] != '\0'; i++) {
+		if((str[i] == '\n') && (str[i + 1] == '\n')) {
+			str[i] = ' ';
+		}
+	}
+	
+	screen_draw_string(str, 8, 25, 0.5f, 0.5f, COLOR_TEXT, false);
 }
