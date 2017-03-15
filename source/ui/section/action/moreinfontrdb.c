5403d948c80041f0c53f0fa178d71233e337eaed
Jolty <Jolty95@users.noreply.github.com>
2017-03-15 11:48:45 +0100

    moreinfontrdb.c: show top screen

--- source/ui/section/action/moreinfontrdb.c
+++ source/ui/section/action/moreinfontrdb.c
@@ -16,16 +16,18 @@
 #include "../../error.h"
 #include "../../../core/screen.h"
 
-void moreinfo_ntrdb_open(linked_list* items, list_item* selected) {
-	ntrdb_info* data = (ntrdb_info*) selected->data;
+ntrdb_info* ntrdbInfo = NULL;
 
-    if(data == NULL) {
+void moreinfo_ntrdb_open(linked_list* items, list_item* selected) {
+	ntrdbInfo = (ntrdb_info*) selected->data;
+
+    if(ntrdbInfo == NULL) {
         error_display(NULL, NULL, "Failed to read NTRDB description data.");
 
         return;
     }
 	
-    list_display(data->meta.name, "B: Return", NULL, ntrdb_update, ntrdb_draw_top);
+    list_display(ntrdbInfo->meta.name, "B: Return", NULL, ntrdb_update, ntrdb_draw_top);
 }
 
 static void ntrdb_update(ui_view* view, void* data, linked_list* items, list_item* selected, bool selectedTouched) {
@@ -36,5 +38,5 @@ static void ntrdb_update(ui_view* view, void* data, linked_list* items, list_ite
 }
 
 static void ntrdb_draw_top(ui_view* view, void* data, float x1, float y1, float x2, float y2, list_item* selected) {
-
+	ui_draw_ntrdb_info(view, ntrdbInfo, x1, y1, x2, y2);
 }
