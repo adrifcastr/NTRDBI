65d37cdccab433fe8d1ba9e6dd7c3fb829520e65
OctoNezd <nezd@protonmail.com>
2017-03-18 23:21:01 +0300

    Actually fixed builds

--- nightly-server/send_build.py
+++ nightly-server/send_build.py
@@ -2,22 +2,27 @@ try:
     from base64 import b64encode
     from os import getenv
     from json import dumps
+    from subprocess import check_output
     import requests
     with open("output/NTRDBI.zip", 'rb') as f:
-        content = b64encode(f.read())
+        content = str(b64encode(f.read()), 'utf-8')
+    version = str(
+        check_output('git log -n 1 --pretty=format:"%h"', shell=True), 'utf-8')
     req = dumps(
-        {"message": "[AUTO]NTRDBI Nightly build updated.",
+        {"message": "[AUTO]NTRDBI Nightly build file for %s" % version,
+          "committer": {
+            "name": "AutoNightly",
+            "email": "noreply@octonezd.pw"
+          },
          "branch": "NTRDBI-Nightlies-Server",
-         "commiter": {
-             "name": "NTRDBI Nightly Builds"
-         },
          "content": content
          }
     )
-    headers = {"Authorization", "token " + getenv("GITHUBAUTH")}
+    headers = {"Authorization": "token " + getenv("GITHUBAUTH")}
     fileurl = "https://api.github.com/repos/adrifcastr/NTRDB-Plugin-Host/contents/%s"
-    r = requests.put(fileurl % ("NTRDBI.zip"), headers=headers)
-except Exception:
+    r = requests.put(fileurl % (version + ".zip"), headers=headers, data=req)
+    print("GitHub returned", r.status_code)
+except Exception as e:
     print("An error occured during uploading nightly build.")
     print(
         "To pretend secure data leaks, no error info was displayed")
