0ad3e7c7a1c853ca15e8cbabd54b78baebdb6f52
OctoNezd <nezd@protonmail.com>
2017-03-19 12:10:42 +0300

    Update file and some modifications in send_build.py

--- nightly-server/send_build.py
+++ nightly-server/send_build.py
@@ -4,24 +4,42 @@ try:
     from json import dumps
     from subprocess import check_output
     import requests
-    with open("output/NTRDBI.zip", 'rb') as f:
-        content = str(b64encode(f.read()), 'utf-8')
+    itemstopush = {
+        "output/NTRDBI.zip": "NTRDBI.zip",
+        "output/3ds-arm/NTRDBI.cia": "latest.cia"
+    }
     version = str(
         check_output('git log -n 1 --pretty=format:"%h"', shell=True), 'utf-8')
-    req = dumps(
-        {"message": "[AUTO]NTRDBI Nightly build file for %s" % version,
-          "committer": {
-            "name": "AutoNightly",
-            "email": "noreply@octonezd.pw"
-          },
-         "branch": "NTRDBI-Nightlies-Server",
-         "content": content
-         }
-    )
-    headers = {"Authorization": "token " + getenv("GITHUBAUTH")}
-    fileurl = "https://api.github.com/repos/adrifcastr/NTRDB-Plugin-Host/contents/%s"
-    r = requests.put(fileurl % (version + ".zip"), headers=headers, data=req)
-    print("GitHub returned", r.status_code)
+    github = "https://api.github.com/repos/adrifcastr/NTRDB-Plugin-Host/"
+    branch = "NTRDBI-Nightlies-Server"
+
+    def upload(file, filename):
+        fileurl = github + "contents/%s"
+        print("Uploading", file, "as", filename, end=" ")
+        reqjs = {"message": "[AUTO]NTRDBI Nightly build %s for %s" % (filename, version),
+                 "committer": {
+                 "name": "Nightly Builds",
+                 "email": "noreply@octonezd.pw"
+        },
+            "branch": branch,
+        }
+        with open(file, 'rb') as f:
+            reqjs["content"] = str(b64encode(f.read()), 'utf-8')
+        tree = requests.get(github + "git/trees/heads/" + branch).json()
+        for file in tree["tree"]:
+            if file["path"] == filename:
+                reqjs["sha"] = file["sha"]
+                break
+        req = dumps(reqjs)
+        headers = {"Authorization": "token " + getenv("GITHUBAUTH")}
+        r = requests.put(fileurl % (filename), headers=headers, data=req)
+        if r.status_code == 201 or r.status_code == 200:
+            print("[OK]")
+        else:
+            print("[FAIL:", r.status_code, "]", sep="")
+
+    for file in itemstopush:
+        upload(file, itemstopush[file])
 except Exception as e:
     print("An error occured during uploading nightly build.")
     print(
